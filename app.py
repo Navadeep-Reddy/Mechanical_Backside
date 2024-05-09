19058d7c30e920f84b77a73358d9548b56ebce85
Navadeep-Reddy <vnavadeepreddysatti@gmail.com>
2024-05-09 22:23:04 +0530

    Commented to improve readability

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ def index():
     global about_name
     global index_get_count   
 
+    #rendering login page when It is the first get request to "/" route and rendering landing page if it's more than one
     if request.method == "GET": 
         if (index_get_count == 0):
             index_get_count += 1
@@ -29,10 +30,16 @@ def index():
         else:
             return render_template("land.html")
     
+    #If its a post request, we get the data from the route and render landing page if correct else render login again with message
     if request.method == "POST":
+        #get username and password from the route
         username = request.form.get("Username", None).strip()
-        about_name = username
         password = request.form.get("Password", None)
+
+        #Storing the username in a global variable to be reused outside this funcion
+        about_name = username
+
+        #Check if username exists in dictionary
         if username in userpass:
             if userpass[username] == password:
                 return render_template("land.html")
@@ -41,19 +48,27 @@ def index():
         else:
             return render_template("index.html", message="User does not exist!")
 
-
+#Created a new route for the about page
 @app.route('/about')
 def about():
     return render_template("about.html", user = about_name)
 
-
+#Created a new route for make a request button
 @app.route("/make_request", methods=['GET', 'POST'])
 def make_request():
+    #Render the page when it's the (first) get request
     if request.method == "GET":
         return render_template("request.html")
 
+    #If It's a post request then It means that submission has occured
     if request.method == "POST":
+
+        #Store the vehicle_type and repair_type in variables
         vehicle_type = request.form.get("vtype", "Did not work")
         repair_type = request.form.get("repair", "Not working")
+
+        #Pass It into the imported function to write It into a file
         orders_log(about_name ,vehicle_type, repair_type)
+
+        #Render the same page with the updated message
         return render_template("request.html", confirm = "Your Order Has Been Noted")
