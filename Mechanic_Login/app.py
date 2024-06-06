e5051bee2b3e12c6751110428474afec5e6221df
Navadeep-Reddy <vnavadeepreddysatti@gmail.com>
2024-06-06 18:24:32 +0530

    Finished Async data transfer

--- Mechanic_Login/app.py
+++ Mechanic_Login/app.py
@@ -1,6 +1,11 @@
 #Importing modules for mechanic login
 from flask import Flask, render_template, request, jsonify, url_for
 
+from Functions.read_prog import alljobcards
+
+import os
+os.chdir(os.path.dirname(os.getcwd()))
+
 #creating 
 app = Flask(__name__)
 
@@ -13,7 +18,23 @@ def orders():
     if request.method == "POST":
 
         name = request.form.get("customer_name")
-        
+        date = request.form.get("date")
+        print(name, date)
 
         return render_template("orders.html", order_url = url_for('orders'))
+    
+
+@app.route("/get_jobs", methods = ["GET"])
+def get_jobs():
+
+    #List for storing all jobcards to send to javascript
+    jobcards =  alljobcards()
+
+    #List for storing the required fields for javascript
+    Data = []
+
+    for card in jobcards:
+        Data.append([card.username, card.vehicle, card.repair, card.reg_no, card.delivery_date])
+
+    return jsonify(Data)
     
