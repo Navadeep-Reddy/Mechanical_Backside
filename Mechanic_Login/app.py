9e22c67c3870a0ac834b9ee56a711975d9c19411
Navadeep-Reddy <vnavadeepreddysatti@gmail.com>
2024-06-06 14:31:59 +0530

    Added new directory for Mechanic Login and got it working with flask

---
+++ Mechanic_Login/app.py
@@ -0,0 +1,15 @@
+#Importing modules for mechanic login
+from flask import Flask, render_template, request, jsonify, url_for
+
+#creating 
+app = Flask(__name__)
+
+
+@app.route('/', methods=["GET", "POST"])
+def orders():
+    if request.method == "GET":
+        return render_template("orders.html")
+    
+    if request.method == "POST":
+        return render_template("orders.html", order_url = url_for('orders'))
+    
